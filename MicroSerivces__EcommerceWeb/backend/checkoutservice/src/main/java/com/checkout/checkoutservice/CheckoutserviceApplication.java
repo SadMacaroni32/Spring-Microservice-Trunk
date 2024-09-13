66474c3c7168a2ca276a4078780734f0b4683aa3
JE <flash.staffsuport@gmail.com>
2024-09-13 14:10:24 +0800

    First Commit

---
+++ MicroSerivces__EcommerceWeb/backend/checkoutservice/src/main/java/com/checkout/checkoutservice/CheckoutserviceApplication.java
@@ -0,0 +1,13 @@
+package com.checkout.checkoutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutserviceApplication.class, args);
+	}
+
+}
---
+++ MicroSerivces__EcommerceWeb/backend/checkoutservice/src/test/java/com/checkout/checkoutservice/CheckoutserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkout.checkoutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
